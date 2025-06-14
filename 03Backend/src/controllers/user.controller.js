2bf45994b64bff84205344918c6a5a5bdc2c54e3
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-15 00:31:25 +0530

    user controllers logic

--- 03Backend/src/controllers/user.controller.js
+++ 03Backend/src/controllers/user.controller.js
@@ -1,9 +1,23 @@
-import {asyncHandler} from '../utils/asyncHandler.js';
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    console.log(("User registration request received"));
+  const { fullName, email, password, username } = req.body;
+  console.log("email : ", email);
+
+  if (!fullName || !email || !password || !username) {
+    throw new ApiError("All fields are required", 400);
+  }
+
+  const existingUser = User.findOne({ email }).then((existingUser) => {
+    if (existingUser) {
+      throw new ApiError("User with this email already exists", 400);
+    }
+  });
+
   res.status(200).json({
-    message: "User registered successfully"
+    message: "User registered successfully",
   });
 });
 
