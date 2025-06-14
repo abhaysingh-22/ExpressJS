1e36fe89cfa9949b0d41477e2b51a3c0fe532b89
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-15 00:31:14 +0530

    routes user

--- 03Backend/src/routes/user.routes.js
+++ 03Backend/src/routes/user.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 
 // both of these lines are equivalent
 // router.post("/register", registerUser);
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverPicture", maxCount: 1 }
+    ]),
+    registerUser
+);
 
 export default router;
