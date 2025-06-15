249bc243900ae7db82477755e13ff49838377be4
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-15 15:11:13 +0530

    logic for registering a user

--- 03Backend/src/controllers/user.controller.js
+++ 03Backend/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
+import cloudinary from "../utils/Cloudinary.js";
+import { APIresponse } from "../utils/APIresponse.js"; 
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password, username } = req.body;
@@ -16,9 +18,43 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   });
 
-  res.status(200).json({
-    message: "User registered successfully",
+  // these below are the properties of multer
+  const avatarPath = req.files?.avatar[0]?.path;
+  const coverImagePath = req.files?.coverImage[0]?.path;
+
+  if (!avatarPath || !coverImagePath) {
+    throw new ApiError("Avatar and cover image are required", 400);
+  }
+
+  const avatarUrl = await cloudinary(avatarPath);
+  const coverImageUrl = await cloudinary(coverImagePath);
+
+  if(!avatarUrl || !coverImageUrl) {
+    throw new ApiError("Error uploading images", 500);
+  }
+
+  const newUser = await User.create({
+    fullName,
+    email,
+    password,
+    username,
+    avatar: avatarUrl || "",
+    coverImage: coverImageUrl || "",
   });
+
+  // if (!newUser) {
+  //   throw new ApiError("User registration failed", 500);
+  // }
+
+  const createdUser = await User.findById(newUser._id).select("-password -refreshToken");
+  if (!createdUser) {
+    throw new ApiError("User not found after creation", 404);
+  }
+
+  
+  return res.status(200).json(
+    new APIresponse(200, "User registered successfully", createdUser)
+  );
 });
 
 export { registerUser };
