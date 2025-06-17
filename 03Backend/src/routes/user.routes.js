de311ced25acc7f713c6133b5ba3ca06c2419acb
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-18 01:14:47 +0530

    expanding from register to login and logout

--- 03Backend/src/routes/user.routes.js
+++ 03Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logoutUser, loginUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -13,4 +13,8 @@ router.route("/register").post(
     registerUser
 );
 
+router.route("/login").post(loginUser)
+
+
+router.route("/logout").post(verifyJWT, loginUser)
 export default router;
