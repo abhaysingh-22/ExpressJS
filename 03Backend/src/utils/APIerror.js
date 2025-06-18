85d47fc0bf622a7c08ccab3f2b8a354c6d678e87
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-19 02:24:42 +0530

    added something API error

--- 03Backend/src/utils/APIerror.js
+++ 03Backend/src/utils/APIerror.js
@@ -1,12 +1,38 @@
-class APIError extends Error {
-    constructor(message, statusCode = 500){
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.success = false;
+// class APIError extends Error {
+//     constructor(statusCode = 500, message){
+//         super(message);
+//         this.statusCode = statusCode;
+//         this.data = null;
+//         this.success = false;
         
-        Error.captureStackTrace(this, this.constructor);
+//         Error.captureStackTrace(this, this.constructor);
+//     }
+// }
+
+// export { APIError };
+
+
+class APIError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
     }
 }
 
-export { APIError };
+export {APIError}
