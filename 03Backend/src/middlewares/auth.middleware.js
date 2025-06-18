784a067e2f7ec8539ac42d7cbc759003262bf33d
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-19 02:23:59 +0530

    logout user debugged successfully

--- 03Backend/src/middlewares/auth.middleware.js
+++ 03Backend/src/middlewares/auth.middleware.js
@@ -1,24 +1,23 @@
 import { APIError } from "../utils/APIerror.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
+
+export const verifyJWT = asyncHandler(async(req, _, next) => {    
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
         
-        // console.log(token);
         if (!token) {
             throw new APIError(401, "Unauthorized request")
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
     
         if (!user) {
-            
-            throw new APIError(401, "Invalid Access Token")
+            throw new APIError (401, "Invalid Access Token")
         }
     
         req.user = user;
@@ -26,5 +25,4 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
     } catch (error) {
         throw new APIError(401, error?.message || "Invalid access token")
     }
-    
 })
