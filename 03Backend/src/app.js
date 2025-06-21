20d986bb113adab086bd236856ebeb9c576842f9
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-21 14:08:16 +0530

    refactor app.js: reorganize middleware setup and import order for improved clarity

--- 03Backend/src/app.js
+++ 03Backend/src/app.js
@@ -1,38 +1,37 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
 
-const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:8000",
     credentials: true,
 }));
 
-app.use(express.json());
-
 app.use(express.urlencoded({
     extended: true,
     limit: "50mb",
 }));
 
+
+const app = express();
+app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
 
 
+import userRoutes from "./routes/user.routes.js";
 
-// Import routes
-//routes declaration
+
 app.use("/api/v1/users", userRoutes);
 
-// Add this at the end of your middleware stack
+
+
 app.use((err, req, res, next) => {
   console.error(err); // This will print the full error in your terminal
   res.status(err.statusCode || 500).json({
     success: false,
     message: err.message || "Internal Server Error",
-    // error: err, // Uncomment for debugging, but remove in production
   });
 });
 
