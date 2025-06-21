2b2dde30c20eb3edc7f0d24b2ed9f7daca64eaba
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-21 14:41:53 +0530

    refactor user.routes.js: reorganize route imports and improve formatting for better readability

--- 03Backend/src/routes/user.routes.js
+++ 03Backend/src/routes/user.routes.js
@@ -1,20 +1,43 @@
 import { Router } from "express";
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js";
-import { verifyJWT } from  "../middlewares/auth.middleware.js";
+import {
+  logoutUser,
+  loginUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+  updateAccountDetail,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router();
 
 
 router.route("/register").post(
-    upload.fields([
-        { name: "avatar", maxCount: 1 },
-        { name: "coverImage", maxCount: 1 }
-    ]),
-    registerUser
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
 );
 
+
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyJWT,  logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refreshToken").post(refreshAccessToken);
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+router.route("/currentUser").get(verifyJWT, getCurrentUser)
+router.route("/updateAccount").patch(verifyJWT, updateAccountDetail)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)     // upload,single helps to accept and process a single image file
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 export default router;
